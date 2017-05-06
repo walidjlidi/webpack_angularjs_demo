fae8e4c46294dcf4e5d49d62b579f1f74d497590
walid <walid.jlidi89@gmail.com>
2017-05-06 18:13:25 +0100

    Activate debug option in webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
+var webpack = require('webpack');
 module.exports = {
     context: __dirname+ '/src',
     entry: './index.js',
     output:{
         path: __dirname + '/dist',
         filename: 'bundle.js'
-    }
+    },
+    plugins:[
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                context : __dirname
+            }
+        })
+    ]
 }
